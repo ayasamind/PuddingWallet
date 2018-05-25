b7c8ddeb24b9a39d9fd1489ab30e2a93338e520f
Tatsuro Kawano <tatsurokawano@TatsuronoMacBook-puro.local>
2018-05-25 12:34:57 +0900

    first pudding commit

---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <Page class="page">
+    <ActionBar class="action-bar" title="Home"/>
+
+    <StackLayout>
+      <Button class="btn btn-primary" @tap="$router.push('/counter')">C</Button>
+      <Button class="btn btn-primary" @tap="$router.push('/hello')">Hello World</Button>
+    </StackLayout>
+
+  </Page>
+</template>
