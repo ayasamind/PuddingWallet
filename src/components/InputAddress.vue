bcf518b110691fdbca07f4df0706e8adc26ff53f
kawano-fusic <kawano@fusic.co.jp>
2018-05-31 23:13:41 +0900

    バーコードリーダー起動ができるように

--- src/components/InputAddress.vue
+++ src/components/InputAddress.vue
@@ -11,7 +11,7 @@
       <TextField v-model="price" />
       <Button class="btn btn-primary" @tap="sendEther()" text="送金！"/>
       <Label class="body" textWrap=true text="バーコードを読み取る"/>
-      <Button class="btn btn-primary" text="カメラ起動"/>
+      <Button class="btn btn-primary" @tap="scanbarcode()" text="カメラ起動"/>
     </StackLayout>    
   </Page>
 </template>
@@ -49,7 +49,7 @@
         scanbarcode() {
             barcodescanner.scan({
                 formats: "QR_CODE,PDF_417",   // Pass in of you want to restrict scanning to certain types
-                cancelLabel: "EXIT. Also, try the volume buttons!", // iOS only, default 'Close'
+                cancelLabel: "Close Barcode Scanner", // iOS only, default 'Close'
                 cancelLabelBackgroundColor: "#333333", // iOS only, default '#000000' (black)
                 message: "Use the volume buttons for extra light", // Android only, default is 'Place a barcode inside the viewfinder rectangle to scan it.'
                 showFlipCameraButton: true,   // default false
@@ -69,30 +69,6 @@
           });
         }
       }
-    //   methods: {
-        //   scanbarcode() {
-        //     barcodescanner.scan({
-        //         formats: "QR_CODE,PDF_417",   // Pass in of you want to restrict scanning to certain types
-        //         cancelLabel: "EXIT. Also, try the volume buttons!", // iOS only, default 'Close'
-        //         cancelLabelBackgroundColor: "#333333", // iOS only, default '#000000' (black)
-        //         message: "Use the volume buttons for extra light", // Android only, default is 'Place a barcode inside the viewfinder rectangle to scan it.'
-        //         showFlipCameraButton: true,   // default false
-        //         preferFrontCamera: false,     // default false
-        //         showTorchButton: true,        // default false
-        //         beepOnScan: true,             // Play or Suppress beep on scan (default true)
-        //         torchOn: false,               // launch with the flashlight on (default false)
-        //         closeCallback: function () { console.log("Scanner closed"); }, // invoked when the scanner was closed (success or abort)
-        //         resultDisplayDuration: 500,   // Android only, default 1500 (ms), set to 0 to disable echoing the scanned text
-        //         orientation: "landscape",     // Android only, optionally lock the orientation to either "portrait" or "landscape"
-        //         openSettingsIfPermissionWasPreviouslyDenied: true // On iOS you can send the user to the settings app if access was previously denied
-        //   }).then((result) => {
-        //     console.log("Scan format: " + result.format);
-        //     console.log("Scan text:   " + result.text);
-        //   },(error) => {
-        //     console.log("No scan: " + error);
-        //   });
-        // }
-//       }
   }
 
 </script>
